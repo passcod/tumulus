daaada780212aab48d824273a5e40f9434b4dc1a
Félix Saparelli <felix@passcod.name>
2026-01-16 00:32:23 +1300

    properly parse and document file extent data

--- crates/btrfs-search/src/items/file_extent.rs
+++ crates/btrfs-search/src/items/file_extent.rs
@@ -1,5 +1,6 @@
 use deku::{ctx::ReadExact, no_std_io, prelude::*};
 
+/// A BTRFS file extent item.
 #[derive(Debug, Clone, PartialEq, DekuRead)]
 pub struct BtrfsFileExtentItem {
     pub header: BtrfsFileExtentItemHeader,
@@ -15,49 +16,128 @@ impl super::SizedItem for BtrfsFileExtentItem {
     const SIZE: usize = 4096;
 }
 
+/// Common metadata for file extent items.
 #[derive(Debug, Clone, PartialEq, DekuRead)]
 pub struct BtrfsFileExtentItemHeader {
+    /// Transaction ID that created this extent.
     #[deku(endian = "little")]
     pub generation: u64,
+
+    /// Max number of bytes to hold this extent in RAM.
+    ///
+    /// When we split a compressed extent we can't know how big each of the resulting pieces will
+    /// be. So, this is an upper limit on the size of the extent in RAM instead of an exact limit.
     #[deku(endian = "little")]
     pub ram_bytes: u64,
-    pub compression: u8,
-    pub encryption: u8,
-    #[deku(endian = "little")]
-    pub other_encoding: u16,
-    pub kind: u8,
+
+    /// Type of compression
+    pub compression: BtrfsCompression,
+
+    /// Type of encryption (currently not used)
+    pub encryption: BtrfsEncryption,
+
+    /// More bits that may be used later for expressing data encoding
+    #[deku(pad_bytes_after = "2")]
+    _other_encoding: (),
+
+    /// Are we inline data or a real (on-disk) extent?
+    pub kind: BtrfsExtentKind,
 }
 
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, DekuRead)]
+#[deku(id_type = "u8", bytes = 1)]
+pub enum BtrfsCompression {
+    #[deku(id = 0)]
+    #[default]
+    None,
+    #[deku(id = 1)]
+    Zlib,
+    #[deku(id = 2)]
+    Lzo,
+    #[deku(id = 3)]
+    Zstd,
+    #[deku(id_pat = "_")]
+    Other { id: u8 },
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, DekuRead)]
+#[deku(id_type = "u8", bytes = 1)]
+pub enum BtrfsEncryption {
+    #[deku(id = 0)]
+    #[default]
+    None,
+    #[deku(id_pat = "_")]
+    Other { id: u8 },
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, DekuRead)]
+#[deku(id_type = "u8", bytes = 1)]
+pub enum BtrfsExtentKind {
+    #[deku(id = 0)]
+    Inline,
+    #[deku(id = 1)]
+    OnDisk,
+    #[deku(id_pat = "_")]
+    Other { id: u8 },
+}
+
+/// Either an inline file body, or its extent-on-disk metadata.
+///
+/// Note the inline buf may be a little longer than the actual file.
 #[derive(Debug, Clone, PartialEq)]
 pub enum BtrfsFileExtentItemBody {
     Inline(Vec<u8>),
     OnDisk(BtrfsFileExtentItemOnDisk),
 }
 
-impl<'a> DekuReader<'a, (u8, u64)> for BtrfsFileExtentItemBody {
+impl<'a> DekuReader<'a, (BtrfsExtentKind, u64)> for BtrfsFileExtentItemBody {
     fn from_reader_with_ctx<R: no_std_io::Read + no_std_io::Seek>(
         reader: &mut Reader<R>,
-        (kind, ram_bytes): (u8, u64),
+        (kind, ram_bytes): (BtrfsExtentKind, u64),
     ) -> Result<Self, DekuError>
     where
         Self: Sized,
     {
         match kind {
-            0 => DekuReader::from_reader_with_ctx(reader, ReadExact(ram_bytes as _))
-                .map(Self::Inline),
-            _ => DekuReader::from_reader_with_ctx(reader, ()).map(Self::OnDisk),
+            BtrfsExtentKind::Inline => {
+                DekuReader::from_reader_with_ctx(reader, ReadExact(ram_bytes as _))
+                    .map(Self::Inline)
+            }
+            BtrfsExtentKind::OnDisk => {
+                DekuReader::from_reader_with_ctx(reader, ()).map(Self::OnDisk)
+            }
+            BtrfsExtentKind::Other { id } => {
+                todo!("unknown extent type {id}, this program cannot safely interpret BTRFS data")
+            }
         }
     }
 }
 
+/// The extent-on-disk metadata.
 #[derive(Debug, Clone, PartialEq, DekuRead)]
 pub struct BtrfsFileExtentItemOnDisk {
+    /// Where the data starts on disk. This is relative to... somewhere, idk.
+    ///
+    /// "At this offset in the structure, the inline extent data start."
     #[deku(endian = "little")]
-    pub disk_bytenr: u64,
+    pub disk_offset: u64,
+
+    /// Disk space consumed by the extent.
+    ///
+    /// Checksum blocks are included in these numbers.
     #[deku(endian = "little")]
-    pub disk_num_bytes: u64,
+    pub disk_bytes: u64,
+
+    /// The logical offset in file blocks (no checksums) this extent record is for.
+    ///
+    /// This allows a file extent to point into the middle of an existing extent on disk, sharing
+    /// it between two snapshots (useful if some bytes in the middle of the extent have changed).
     #[deku(endian = "little")]
-    pub offset: u64,
+    pub logical_offset: u64,
+
+    /// The logical number of file blocks (no checksums included).
+    ///
+    /// This always reflects the size uncompressed and without encoding.
     #[deku(endian = "little")]
-    pub num_bytes: u64,
+    pub logical_bytes: u64,
 }
