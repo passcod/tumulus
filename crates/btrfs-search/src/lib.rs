069dc10fa16760e1ceb1fc4a8288037112db4798
Félix Saparelli <felix@passcod.name>
2026-01-18 13:41:04 +1300

    remove old btrfs-search crate

--- crates/btrfs-search/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// `File` is not supported with Miri's default isolation, so use MIRIFLAGS="-Zmiri-disable-isolation"
-
-mod items;
-mod results;
-mod search;
-
-pub use items::*;
-pub use results::*;
-pub use search::*;
